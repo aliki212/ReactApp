0a0b5d94cedbdaabbe764ca12158ee861e82403c
aliki212 <aliki212@gmail.com>
2020-05-20 18:06:41 +0300

    BootstrapStillNotWorkingCDNthoDo

--- app/main.js
+++ app/main.js
@@ -1,11 +1,38 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import $ from "jquery"
+import Popper from "popper.js"
+import "bootstrap/dist/js/bootstrap.bundle.min"
 
 function BillySFirstComponent() {
   return (
     <>
-      <h1>Hello Billie</h1>
+      <h1 className="text-warning">Hello Wonderland</h1>
       <p>Here we will begin our app.</p>
+      <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+        Launch demo modal
+      </button>
+      <div className="modal-dialog" role="document">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title">Modal title</h5>
+            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div className="modal-body">
+            <p>Modal body text goes here.</p>
+          </div>
+          <div className="modal-footer">
+            <button type="button" className="btn btn-secondary" data-dismiss="modal">
+              Close
+            </button>
+            <button type="button" className="btn btn-primary">
+              Save changes
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
